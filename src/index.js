da66ea7b4eae43eaad2778f4931175ae5193f7d4
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-02-19 21:54:59 +0300

    init: RS task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    if (n < 0) {
+        n = -n;
+    }
+
+    let stringNum = n.toString().split("").reverse().join("");
+    let reversedStringNum = Number(stringNum);
+    return reversedStringNum;
+};
